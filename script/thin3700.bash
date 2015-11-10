b34647b62e487f4e9bc6ec40902d2a8f1369eaf0
ann <ann@ann.edu>
2015-11-10 00:49:54 +0000

    more

--- script/thin3700.bash
+++ script/thin3700.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# ~/histo/rb/histo/script/thin3700.bash
+# ~ann/app26r/script/thin3700.bash
 
-cd ~/histo/rb/histo/
-
+cd ~/app26r/
+.  ~/app26r/app26r.bash
 bin/thin -p 3700 start
 
 exit
